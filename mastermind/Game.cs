a71b9c0b8835defc4ceb3ecf45f1b9946b540c81
ogendogen <kosmos500@interia.pl>
2017-03-28 21:18:09 +0200

    Commit
    Commit

---
+++ mastermind/Game.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Mastermind
+{
+    class Game
+    {
+        // Singleton
+        private static Game Instance = null;
+        private Game() { }
+        public static Game getInstance()
+        {
+            if (Instance == null) Instance = new Game();
+            return Instance;
+        }
+
+        private static readonly Random rand = new Random();
+        private List<int> test; // szukany układ
+        private List<List<int>> questions; // zaproponowany układ
+        private List<List<int>> answers; // odpowiedzi
+        private bool is_initialized = false; // gra zainicjowana
+        private int current; // aktualna pozycja
+        private int colors; // ilosc kolorow, domyslnie 8
+        private const int max_turns = 12; // maksymalna ilość ruchów
+        private int current_turns; // aktualna ilość ruchów 
+
+        public void init() // inicjacja gry, uruchamiamy tylko raz
+        {
+            if (is_initialized) return;
+            test = new List<int>(4);
+            // stworzenie dwuwymiarowych tablic
+            questions = new List<List<int>>(12);
+            answers = new List<List<int>>(12);
+            for (int i=0; i<12; i++)
+            {
+                questions.Add(new List<int>(4));
+                answers.Add(new List<int>(4));
+            }
+            is_initialized = true;
+        }
+
+        public void start() // start gry
+        {
+            current = 0;
+            colors = 8;
+            current_turns = 12;
+            test.Clear();
+
+            for (int i = 0; i < 12; i++)
+            {
+                questions[i].Clear();
+                answers[i].Clear();
+                for (int j = 0; j < 4; j++)
+                {
+                    //questions[i][j] = 0;
+                    //answers[i][j] = 0;
+                    questions[j].Add(0);
+                    answers[j].Add(0);
+                }
+            }
+
+            generateTest();
+        }
+
+        public void printQuestions()
+        {
+            Debug.WriteLine(questions.Count());
+        }
+
+        private void generateTest() // wylosuj test
+        {
+            if (test == null) throw new Exception("Gra nie jest zainicjowana!");
+            for (int i = 0; i < 4; i++) test.Add(rand.Next(1, colors));//test[i] = rand.Next(1, colors);
+        }
+
+        private List<int> getLastQuestion() // pobierz ostatnio zaproponowany układ
+        {
+            return questions.Last(x => x.All(y => y != 0)); // ostatni gdzie wszystkie różne od zera => ostatnio wysłana próba / ostatnia lista bez zer
+        }
+
+        public bool isWin() // czy wygrana ?
+        {
+            return getLastQuestion().SequenceEqual(test); // dwie struktury z dokładnie taką samą zawartością i identyczną kolejnością
+        }
+
+        public bool isLose() // czy przegrana ?
+        {
+            //var is_used_all_slots = questions.All(x => x.All(y => y != 0));
+            //var first_empty = questions.First(x => x.All(y => y != 0)); // pierwszy z samymi zerami => jeżeli takowy istnieje to gra toczy się dalej
+            //return first_empty == null ? true : false;
+            return getCurrentTurns() == 0 ? true : false;
+        }
+
+        public List<int> getLastAnswer() // struktura odpowiedzi: 0 - brak informacji, 1 - istnieje ale na innym miejscu, 2 - trafiony
+        {
+            return answers.Last();
+        }
+
+        private void generateAnswer() // struktura odpowiedzi: 0 - brak informacji, 1 - istnieje ale na innym miejscu, 2 - trafiony
+        {
+            var last_question = getLastQuestion();
+            List<int> answer = new List<int>(4)
+            {
+                0,
+                0,
+                0,
+                0
+            };
+
+            for (int i=0; i<4; i++)
+            {
+                if (last_question[i] == test[i]) answer[i] = 2;
+            }
+
+            for (int i=0; i<4; i++)
+            {
+                for (int j=0; j<4; j++)
+                {
+                    if (answer[i] != 2 && last_question[i] == test[j] && i != j) answer[i] = 1;
+                }
+            }
+
+            if (answer.Count() != 4) throw new Exception("Błąd! Odpowiedź różna od 4 !");
+            answers.Add(answer);
+            current_turns--;
+        }
+
+        public void addQuestion(string question)
+        {
+            if (question == null) throw new Exception("Puste zapytanie !");
+            if (question.Length != 4) throw new Exception("Zapytanie musi mieć dokładnie 4 liczby!");
+            var is_digits_only = question.All(x => x >= '1' && x <= '8');
+            if (!is_digits_only) throw new Exception("Zapytanie może zawierać tylko liczby z zakresu 1-8!");
+
+            //questions.Add(question.Cast<int>().ToList()); // mozliwe uzycie arraylist
+            List<int> i_question = new List<int>(4);
+            foreach (char sign in question) i_question.Add((int)sign - 48); // 49 to '1'
+            questions.Add(i_question);
+            current++;
+
+            generateAnswer();
+        }
+
+        public int getCurrentTurns()
+        {
+            return this.current_turns;
+        }
+    }
+}
---
+++ mastermind/Program.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace Mastermind
+{
+    class Program
+    {
+        public static void updateTurns(int turns)
+        {
+            // zapis aktualnej pozycji
+            int old_top = Console.CursorTop;
+            int old_left = Console.CursorLeft;
+
+            // ustawiamy się na pozycji licznika i czyścimy go
+            Console.CursorTop = 0;
+            Console.CursorLeft = Console.WindowWidth - 3;
+            Console.Write(" ");
+            Console.Write(" ");
+
+            // w wolne pole wpisujemy nową wartość
+            Console.CursorLeft = Console.WindowWidth - 3;
+            Console.Write(turns);
+
+            // powrót na starą pozycję
+            Console.CursorTop = old_top;
+            Console.CursorLeft = old_left;
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Title = "Mastermind";
+                Console.WriteLine("Gra Mastermind\nSzare gwiazdki - brak\nŻółte gwiazdki - złe miejsce\nZielone gwiazdki - poprawne miejsce");
+                Console.ReadKey();
+                Console.Clear();
+
+                Game game = Game.getInstance();
+                game.init();
+                game.start();
+                Console.CursorLeft = Console.WindowWidth - 10; // RUCHY:_XY_
+                Console.Write("Ruchy: " + game.getCurrentTurns());
+                Console.CursorLeft = 0;
+                Console.CursorTop = 0;
+                StringBuilder question = new StringBuilder();
+                while(true) // główna pętla gry
+                {
+                    question.Clear();
+                    for (int i=0; i<4; i++) // zbieranie kolorów do pytania
+                    {
+                        ConsoleKeyInfo c = Console.ReadKey(true);
+                        while (c.KeyChar < '1' || c.KeyChar > '8')
+                        {
+                            if (Console.CursorLeft > 0)
+                            {
+                                Console.Write(" ");
+                                Console.CursorLeft--;
+                            }
+                            c = Console.ReadKey(true);
+                        }
+                        Console.Write(c.KeyChar);
+                        question.Append(c.KeyChar);
+                        Console.CursorLeft += 2;
+                    }
+                    Console.Write("|");
+                    Console.CursorLeft += 2;
+                    try
+                    {
+                        game.addQuestion(question.ToString());
+                    }
+                    catch(Exception e)
+                    {
+                        Console.WriteLine(e.ToString());
+                    }
+
+                    List<int> answer = game.getLastAnswer();
+                    for (int i=0; i<4; i++) // wydrukowanie odpowiedzi
+                    {
+                        Console.ResetColor();
+                        switch (answer[i])
+                        {
+                            case 0:
+                                Console.ForegroundColor = ConsoleColor.Gray;
+                                Console.Write('*');
+                                break;
+                            case 1:
+                                Console.ForegroundColor = ConsoleColor.Yellow;
+                                Console.Write('*');
+                                break;
+                            case 2:
+                                Console.ForegroundColor = ConsoleColor.Green;
+                                Console.Write('*');
+                                break;
+                        }
+                        Console.CursorLeft += 2;
+                    }
+                    
+                    Console.ResetColor();
+                    Console.CursorLeft = 0;
+                    Console.CursorTop++;
+                    if (game.isWin())
+                    {
+                        MessageBox.Show("Odgadłeś układ!", "Zwycięstwo!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        Console.Clear();
+                        Console.CursorTop = 0;
+                        Console.CursorLeft = 0;
+                        game.start();
+                        Console.CursorLeft = Console.WindowWidth - 10; // RUCHY:_XY_
+                        Console.Write("Ruchy: " + game.getCurrentTurns());
+                        Console.CursorLeft = 0;
+                        Console.CursorTop = 0;
+                        continue;
+                    }
+                    if (game.isLose())
+                    {
+                        MessageBox.Show("Koniec gry! Przegrałeś!", "Przegrana!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        Console.Clear();
+                        Console.CursorTop = 0;
+                        Console.CursorLeft = 0;
+                        game.start();
+                        Console.CursorLeft = Console.WindowWidth - 10; // RUCHY:_XY_
+                        Console.Write("Ruchy: " + game.getCurrentTurns());
+                        Console.CursorLeft = 0;
+                        Console.CursorTop = 0;
+                        continue;
+                    }
+                    updateTurns(game.getCurrentTurns());
+                }
+            }
+            catch(Exception e)
+            {
+                Debug.WriteLine(e.StackTrace);
+                MessageBox.Show(e.Message, "Wyjątek!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
